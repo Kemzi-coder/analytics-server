6a220060acc0c6ec57ec65dc6bd755e751b4f415
Kemzi <kemzi9585@gmail.com>
2021-10-27 20:12:53 +0300

    Changes

--- models/User/UserController.js
+++ models/User/UserController.js
@@ -5,8 +5,8 @@ class UserController {
 	async create(req, res) {
 		try {
 			const ip = parseIp(req)
-			const location = await fetch(`http://ip-api.com/json/?fields=${ip}`)
-				.then(res => res.json())
+			const locationJSON = await fetch(`http://ip-api.com/json/?fields=${ip}`)
+			const location = await locationJSON.json()
 			console.log(location)
 			const user = await UserService.create({...req.body, ip})
 			res.json(user)
