1285525309c912e1b1379415174932ab031d2424
Kemzi <kemzi9585@gmail.com>
2021-10-27 21:05:13 +0300

    Some changes

--- utils.js
+++ utils.js
@@ -2,5 +2,5 @@ export const parseIp = req => req.headers['x-forwarded-for']?.split(',')
 	.shift() || req.socket?.remoteAddress
 
 export const generateLocation = res => `
-	${res.continent_code}|${res.country_code} 
-	${res.region === res.city ? res.city : res.region + '/' + res.city}` || ''
+	${res.continent_code}|${res.country_code} ${res.region === res.city ?
+	res.city : res.region + '/' + res.city}` || ''
